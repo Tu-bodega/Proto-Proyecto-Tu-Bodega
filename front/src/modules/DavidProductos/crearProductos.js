161cfca0f3997f447c83bb0f2166df3517310d2a
David <jd159@outlook.com>
2024-03-03 08:11:41 -0500

    ingreso nuevas funcionalidades productos

--- front/src/modules/DavidProductos/crearProductos.js
+++ front/src/modules/DavidProductos/crearProductos.js
@@ -33,75 +33,83 @@ function CrearProductos() {
                 text: msg,
                 icon: 'success',
                 confirmButtonText: 'OK'
+            })
+        .catch((error) => {
+                Swal.fire({
+                    title: 'Error',
+                    text: "no se pudo agregar el producto",
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                });
             });
-        });
-    };
-    return (
-        <div className="containerProductos">
+    });
+};
+return (
+    <div className="containerProductos">
 
-            <form className="productosAgregar"onSubmit={agregarProductos}>
+        <form className="productosAgregar" onSubmit={agregarProductos}>
 
-                <label>
-                    Nombre:
-                    <input onChange={(event) => {
-                        setNombre(event.target.value);
-                    }} type='text' ></input>
-                </label>
+            <label>
+                Nombre:
+                <input onChange={(event) => {
+                    setNombre(event.target.value);
+                }} type='text' ></input>
+            </label>
 
-                <label>
-                    Descripcion:
-                    <textarea onChange={(event) => {
-                        setDescripcion(event.target.value);
-                    }} type='text' />
-                </label>
+            <label>
+                Descripcion:
+                <textarea onChange={(event) => {
+                    setDescripcion(event.target.value);
+                }} type='text' />
+            </label>
 
-                <label>
-                    Precio de compra:
-                    <input onChange={(event) => {
-                        setPrecioC(event.target.value);
-                    }} type='number' ></input>
-                </label>
+            <label>
+                Precio de compra:
+                <input onChange={(event) => {
+                    setPrecioC(event.target.value);
+                }} type='number' ></input>
+            </label>
 
-                <label>
-                    Precio de venta:
-                    <input onChange={(event) => {
-                        setPrecioV(event.target.value);
-                    }} type='number' ></input>
-                </label>
+            <label>
+                Precio de venta:
+                <input onChange={(event) => {
+                    setPrecioV(event.target.value);
+                }} type='number' ></input>
+            </label>
 
-                <label>
-                    Unidades:
-                    <input onChange={(event) => {
-                        setUnidades(event.target.value);
-                    }} type='number' ></input>
-                </label>
+            <label>
+                Unidades:
+                <input onChange={(event) => {
+                    setUnidades(event.target.value);
+                }} type='number' ></input>
+            </label>
 
-                <label>
-                    Fecha:
-                    <input onChange={(event) => {
-                        setFecha(event.target.value);
-                    }} type='date' ></input>
-                </label>
+            <label>
+                Fecha:
+                <input onChange={(event) => {
+                    setFecha(event.target.value);
+                }} type='date' ></input>
+            </label>
 
-                <label>
-                    Unidad de Medida ID:
-                    <input onChange={(event) => {
-                        setMedida(event.target.value);
-                    }} type='number' ></input>
-                </label>
+            <label>
+                Unidad de Medida ID:
+                <input onChange={(event) => {
+                    setMedida(event.target.value);
+                }} type='number' ></input>
+            </label>
 
-                <label>
-                    Proveedor ID:
-                    <input onChange={(event) => {
-                        setProveedor(event.target.value);
-                    }} type='number' ></input>
-                </label>
+            <label>
+                Proveedor ID:
+                <input onChange={(event) => {
+                    setProveedor(event.target.value);
+                }} type='number' ></input>
+            </label>
 
-                <button className='btnRegistrar' type='submit'>Registrar Producto</button>
-            </form>
+            <button className='btnRegistrar' type='submit'>Registrar Producto</button>
+        </form>
 
-        </div>
-    );
+    </div>
+);
 }
 
 export default CrearProductos;
--- front/src/modules/DavidProductos/listaProductos.js
+++ front/src/modules/DavidProductos/listaProductos.js
@@ -2,16 +2,51 @@ import { useState } from 'react';
 import Axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../css/Productos.css";
+import Swal from 'sweetalert2';
 
 function ListaProductos() {
+
     const [productosLista, setProductosLista] = useState([]);
 
+    /*const para utilizar el boton de actualizar produucto*/
+    const productoActualizar = (producto) =>{
+        console.log("Actualizar producto:", producto);
+    };
+
+     /*logica para utilizar el boton de eliminar produucto*/
+    const productoEliminar = (producto) => {
+        const confirmacion = window.confirm(`¿Estás seguro de que deseas eliminar el producto ${producto.nombre_producto}?`);
+
+        if (confirmacion) {
+            Axios.delete(`http://localhost:3001/productos/eliminar/${producto.id}`)
+                .then((response) => {
+                    const msg = "Producto Eliminado"
+                    Swal.fire({
+                        title: 'EXITO',
+                        text: msg,
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    });
+                    leerProductos();
+                })
+                .catch((error) => {
+                    Swal.fire({
+                        title: 'Error',
+                        text: "no se pudo eliminar el producto",
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                    });
+                });
+        };
+    };
+
+    /* funcion para leer los productos de la base de datos*/
     function leerProductos() {
         Axios.get("http://localhost:3001/productos/leer").then((response) => {
             setProductosLista(response.data);
         });
     }
-
+    /* return para traer la informacion en una tabla*/
     return (
         <div className='listaProductos'>
             
@@ -41,6 +76,10 @@ function ListaProductos() {
                                 <td>{new Date(valor.fecha_producto).toLocaleDateString('es')}</td>
                                 <td>{valor.unidades_medida_id}</td>
                                 <td>{valor.proveedores_id}</td>
+                                <td>
+                                    <button onClick={()=> productoActualizar(valor)}>Actualizar</button>
+                                    <button onClick={()=> productoEliminar(valor)}>Eliminar</button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
