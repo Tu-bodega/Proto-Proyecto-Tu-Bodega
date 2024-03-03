18b662bbae59569c09e2e70f760faa4e0000bc2c
Camilo Castillo <cristian.c.castillo1707@gmail.com>
2024-03-03 09:51:01 -0500

    sin errores

--- front/src/modules/CamiloUsers/crearUsuarios.js
+++ front/src/modules/CamiloUsers/crearUsuarios.js
@@ -1,55 +1,58 @@
 import "../../css/Usuarios.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
-import  Axios  from "axios";
+import Axios from "axios";
 
 function CrearUsuario() {
-    const [nombre,setNombre] = useState("");
-    const [apellido,setApellido] = useState("");
-    const [contra,setContra] = useState("");
-    const [contraDos,setContraDos] = useState("");
-    const [correo,setCorreo] = useState("");
-    const [rol,setRol] = useState(0);
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [contra, setContra] = useState("");
+    const [contraDos, setContraDos] = useState("");
+    const [correo, setCorreo] = useState("");
+    const [rol, setRol] = useState(0);
 
-    const agregar = (e)=>{
+    const agregar = (e) => {
         e.preventDefault();
-        Axios.post("http://localhost:3001/empleados/crear",{
-            nombre:nombre,
-            apellido:apellido,
-            contra:contra,
-            correo:correo,
-            rol:rol,
-        }).then(()=>{
+        Axios.post("http://localhost:3001/empleados/crear", {
+            nombre: nombre,
+            apellido: apellido,
+            contra: contra,
+            correo: correo,
+            rol: rol,
+        }).then(() => {
             alert("Usuario agregado")
         });
     };
-    return(
+    return (
         <div className="containerUsers">
             <form onSubmit={agregar}>
 
                 <label> Nombre:
-                    <input onChange={(event)=>{setNombre(event.target.value)}} type="text"></input>
+                    <input onChange={(event) => { setNombre(event.target.value) }} type="text"></input>
                 </label>
 
                 <label> Apellido:
-                    <input onChange={(event)=>{setApellido(event.target.value)}} type="text"></input>
+                    <input onChange={(event) => { setApellido(event.target.value) }} type="text"></input>
                 </label>
 
                 <label> Contraseña:
-                    <input onChange={(event)=>{setContra(event.target.value)}} type="text"></input>
+                    <input onChange={(event) => { setContra(event.target.value) }} type="text"></input>
                 </label>
 
                 <label> Repetir Contraseña:
-                    <input onChange={(event)=>{setContraDos(event.target.value)}} type="text"></input>
+                    <input onChange={(event) => { setContraDos(event.target.value) }} type="text"></input>
                 </label>
 
                 <label> Correo:
-                    <input onChange={(event)=>{setCorreo(event.target.value)}} type="text"></input>
+                    <input onChange={(event) => { setCorreo(event.target.value) }} type="text"></input>
                 </label>
 
-                <label> Rol:
-                    <input onChange={(event)=>{setRol(event.target.value)}} type="number"></input>
-                </label>
+                <select className="input select" value={rol} required
+                    onChange={(event) => { setRol(event.target.value); }}>
+                    <option value="" hidden>Seleccione su rol</option>
+                    <option value="1">Administrador</option>
+                    <option value="2">Cajero</option>
+                </select>
 
                 <button type="submit">Agregar</button>
             </form>
