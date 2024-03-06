c4d86899acf6a24810d731958f63e91c17058fff
Camilo Castillo <cristian.c.castillo1707@gmail.com>
2024-03-06 09:23:44 -0500

    implementacion se Swagger para documentar la api

--- back/src/index.js
+++ back/src/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mysql from "mysql";
 import myCon from "express-myconnection";
 import cors from "cors";
+import swaggerJSDOCs from "../swagger.js";
 import configPuerto from "./configuraciones/configPuerto.js";
 import configDb from "./configuraciones/configDataBase.js";
 import rutaProductos from "./routes/rutaProductos.js";
@@ -11,15 +12,14 @@ import rutaProveedores from "./routes/rutaProveedores.js";
 
 const app = express();
 
-app.listen(configPuerto.puerto, ()=>{
-    console.info(`http://localhost:${configPuerto.puerto}/`);
-    console.info(`http://localhost:${configPuerto.puerto}/productos/leer`);
-});
-
 app.use(bodyParser.json());
 app.use(cors());
 app.use(myCon(mysql, configDb.mysql,'single'));
 
+app.listen(configPuerto.puerto, ()=>{
+    console.info(`server escuchando en el puerto${configPuerto.puerto}/`);
+    swaggerJSDOCs(app, configPuerto.puerto);
+});
 
 app.get("/",(req,res)=>{
     res.send(`Estoy en el puerto ${configPuerto.puerto}`)
@@ -30,3 +30,4 @@ app.use("/login", rutaEmpleado);
 app.use("/empleados", rutaEmpleado);
 app.use("/productos", rutaProductos);
 app.use("/proveedores", rutaProveedores);
+
--- back/src/routes/rutaEmpleados.js
+++ back/src/routes/rutaEmpleados.js
@@ -1,8 +1,155 @@
-import express from "express"
+import express, { application } from "express"
 import bcrypt from "bcryptjs";
 
 
 const rutaEmpleado = express.Router();
+/** 
+*@swagger
+*components:
+*    schemas:
+*        empleados:
+*            type: object
+*            properties:
+*                id:
+*                    type: integer
+*                    description: Id autogenerado en la base de datos
+*                nombre_empleado:
+*                    type: string
+*                    description: nombre del empleado
+*                apellido_empleado:
+*                    type: string
+*                    description: apellido del empleado
+*                password_empleado: 
+*                    type: string
+*                    description: contraseña de ingreso empleado
+*                correo_empleado:
+*                    type: string
+*                    description: correo empleado
+*                rol_empleados_id:
+*                    type: integer
+*                    description: llave foranea con tabla rol_empleados 1 para administrador 2 para cajero
+*            required:
+*                -   nombre_empleado
+*                -   apellido_empleado
+*                -   password_empleado
+*                -   correo_empleado
+*                -   rol_empleados_id
+*            example:
+*                nombre_empleado: camilo 
+*                apellido_empleado: castillo
+*                password_empleado: Contraseña12345
+*                correo_empleado: camil-code@gmail.com
+*                rol_empleados_id: 1
+*/
+
+/**
+ * @openapi
+ * /empleados//leer:
+ *   get:
+ *     summary: Lee todos los empleados
+ *     tags: [empleados]
+ *     description: Obtiene una lista de todos los empleados de la base de datos.
+ *     responses:
+ *       200:
+ *         description: Una lista de empleados.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: El ID único del empleado.
+ *                   nombre:
+ *                     type: string
+ *                     description: El nombre del empleado.
+ *                   correo:
+ *                     type: string
+ *                     description: El correo electrónico del empleado.
+ *                   cargo:
+ *                     type: string
+ *                     description: El cargo del empleado en la empresa.
+ *       400:
+ *         description: Tabla no encontrada o error en la consulta.
+ *       500:
+ *         description: Error en conexión con la base de datos.
+ */
+
+rutaEmpleado.get("/leer",(req,res)=>{
+    req.getConnection((error,conexion)=>{
+        error? res.status(500).json({error:'Error en conexion con la base de datos'})
+        :conexion.query("SELECT * FROM empleados",(err,resultado)=>{
+            err? res.status(400).json({err:'tabla no encontrada'})
+            :res.send(resultado)
+        });
+    });
+});
+
+/**
+* @swagger
+* /empleados/validar:
+*   post:
+*     summary: Valida las credenciales del empleado.
+*     tags: [empleados]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               rol:
+*                 type: integer
+*                 description: El ID del rol del empleado.
+*               correo:
+*                 type: string
+*                 description: El correo electrónico del empleado.
+*               contra:
+*                 type: string
+*                 description: La contraseña del empleado.
+*             required:
+*               - rol
+*               - correo
+*               - contra
+*     responses:
+*       200:
+*         description: Autenticación exitosa, saludos al empleado.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 saludos:
+*                   type: string
+*                   description: Nombre del empleado.
+*                 mensaje:
+*                   type: string
+*                   description: Mensaje de bienvenida.
+*       401:
+*         description: Autenticación fallida debido a credenciales inválidas.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 error:
+*                   type: string
+*                   description: Mensaje de error indicando credenciales incorrectas.
+*       500:
+*         description: Error de conexión con la base de datos.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 error:
+*                   type: string
+*                   description: Mensaje de error indicando fallo en la conexión.
+*/
+
+
 
 rutaEmpleado.post("/validar", (req, res) => {
     const rol = parseInt(req.body.rol);
@@ -40,6 +187,77 @@ rutaEmpleado.post("/validar", (req, res) => {
 });
 
 
+/**
+* @swagger
+* /empleados/crear:
+*   post:
+*     summary: Crea un nuevo empleado.
+*     tags: [empleados]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               nombre:
+*                 type: string
+*                 description: El nombre del empleado.
+*               apellido:
+*                 type: string
+*                 description: El apellido del empleado.
+*               contra:
+*                 type: string
+*                 description: La contraseña del empleado.
+*               correo:
+*                 type: string
+*                 description: El correo electrónico del empleado.
+*               rol:
+*                 type: integer
+*                 description: El ID del rol del empleado.
+*             required:
+*               - nombre
+*               - apellido
+*               - contra
+*               - correo
+*               - rol
+*     responses:
+*       201:
+*         description: Usuario creado con éxito.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 mensaje:
+*                   type: string
+*                   description: Mensaje de éxito al crear el usuario.
+*                 resultado:
+*                   type: object
+*                   description: Detalles del resultado de la inserción.
+*       409:
+*         description: Conflicto por correo electrónico ya registrado.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 mensaje:
+*                   type: string
+*                   description: Mensaje indicando que el correo ya está registrado.
+*       500:
+*         description: Error de conexión con la base de datos o error al verificar existencia del usuario.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 error:
+*                   type: string
+*                   description: Mensaje de error indicando el problema.
+*/
+
+
 rutaEmpleado.post("/crear", async (req, res) => {
     const { nombre, apellido, contra, correo } = req.body;
     const rol = parseInt(req.body.rol);
@@ -73,24 +291,7 @@ rutaEmpleado.post("/crear", async (req, res) => {
 });
 
 
-rutaEmpleado.get("leer",(req,res)=>{
-    req.getConnection((error,conexion)=>{
-        error? res.status(500).json({error:'Error en conexion con la base de datos'})
-        :conexion.query("SELECT * FROM empleados",(err,resultado)=>{
-            err? res.status(400).json({err:'tabla no encontrada'})
-            :res.send(resultado)
-        });
-    });
-});
 
-rutaEmpleado.put("actualizar",async(req,res)=>{
-    const nombre = String(req.body.nombre);
-    const apellido = String(req.body.apellido);
-    const contra = String(req.body.contra);
-    const correo = String(req.body.correo);
-    const rol = parseInt(req.body.rol);
-    const encriptacion = await bcrypt.hash(contra, 8);
-    
-});
+
 
 export default rutaEmpleado;
